0b65c7a6af5ff08dfcca63815c6882d43fed361c
cczyWyc <274230928@qq.com>
2022-11-24 23:38:23 +0800

    redis-lock: add unit test

--- demo/lock_demo_test.go
+++ demo/lock_demo_test.go
@@ -2,13 +2,18 @@ package demo
 
 import (
 	"context"
+	"distributed_lock-go/mocks"
+	"errors"
 	"github.com/go-redis/redis/v9"
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
 )
 
 func TestClient_TryLock(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 	testCases := []struct {
 		name string
 		mock func() redis.Cmdable
@@ -19,7 +24,50 @@ func TestClient_TryLock(t *testing.T) {
 		wantErr  error
 		wantLock *Lock
 	}{
-		{},
+		{
+			name:       "locked",
+			key:        "locked-key",
+			expiration: time.Minute,
+			mock: func() redis.Cmdable {
+				rdb := mocks.NewMockCmdable(ctrl)
+				res := redis.NewBoolResult(true, nil)
+				rdb.EXPECT().
+					SetNX(gomock.Any(), "locked-key", gomock.Any(), time.Minute).
+					Return(res)
+				return rdb
+			},
+			wantLock: &Lock{
+				key: "locked-key",
+			},
+		},
+		{
+			name:       "network error",
+			key:        "network-key",
+			expiration: time.Minute,
+			mock: func() redis.Cmdable {
+				rdb := mocks.NewMockCmdable(ctrl)
+				res := redis.NewBoolResult(false, errors.New("network error"))
+				rdb.EXPECT().
+					SetNX(gomock.Any(), "network-key", gomock.Any(), time.Minute).
+					Return(res)
+				return rdb
+			},
+			wantErr: errors.New("network error"),
+		},
+		{
+			name:       "failed to lock",
+			key:        "failed-key",
+			expiration: time.Minute,
+			mock: func() redis.Cmdable {
+				rdb := mocks.NewMockCmdable(ctrl)
+				res := redis.NewBoolResult(false, nil)
+				rdb.EXPECT().
+					SetNX(gomock.Any(), "failed-key", gomock.Any(), time.Minute).
+					Return(res)
+				return rdb
+			},
+			wantErr: ErrFailedToPreemptLock,
+		},
 	}
 
 	for _, tc := range testCases {
